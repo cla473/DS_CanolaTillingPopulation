94519a88965d3993d719bfa51e0ece320eef3efb
cla473 <Loretta.Clancy@csiro.au>
2018-05-11 13:33:17 +1000

    Updated version of script

--- scripts/LC_GetHeaderDetails.sh
+++ scripts/LC_GetHeaderDetails.sh
@@ -1,45 +1,46 @@
 #!/bin/bash
 
-#SBATCH --job-name=:LC_GetHeaderDetails
+#SBATCH --job-name=:GetHeaderDetails
 #SBATCH --time=00:10:00
 #SBATCH --nodes=-1
 #SBATCH --ntasks-per-node=5
 #SBATCH --mem=2g
 
-# Application specific commands
-INDIR="/OSM/CBR/AF_DATASCHOOL/input/2018-05-03_canola/NB501086_0095_CHelliwell_CSIRO_Brapa_gDNA/*.gz"
+#define the dicrectory we are going to work with
+#added */* to be able to choose all files from 2018-05-03_canola folder
+INDIR="/OSM/CBR/AF_DATASCHOOL/input/2018-05-03_canola/*/*.gz"
 
+echo "Process started...."
 
 #add a header row to our header.txt file
-echo "id, filename, name, name, name" > headers.txt
+echo "index, poolNo, sampleNo, repNo, barcode, laneNo, filename" > headers.txt
 
-let count=0
 
-for file in $INDIR
+for FILE in $INDIR
 do
-    #increment our count, so we can output it
-    ((count++))
-    #echo "$file"
-    echo $count", " $(basename "$file") >> headers.txt
+
+    #get the filename (remove path information)
+    FILENAME=`echo $(basename "$FILE")`
+    #echo "File: $FILENAME"
 
     #now get the header from the file
-    #HEADER=`head -1 $file | gunzip` - this gives unexpected end of file erros
-    HEADER=`zcat $file | head -n1`
-    #echo "  "$HEADER >> headers.txt
+    HEADER=`zcat $FILE | head -n1`
 
     #now get the portion of the header that we want
-    #"@NB501086:95:H72NFAFXX:1:11101:17128:1036 1:N:0:CGATGT"
+    #eg. @NB501086:95:H72NFAFXX:1:11101:17128:1036 1:N:0:CGATGT
+    INDEX=`echo $HEADER | cut -d ':' -f 10 `
+    BARCODE=`echo $HEADER | cut -d ':' -f 3 `
+    LANE=`echo $HEADER | cut -d ':' -f 4 `
 
-    #DETAILS=`$HEADER | cut -d ':' -f 3,4,10 --output-delimiter=', '`
-    #echo $DETAILS >> headers.txt
+    #to cut the pool number out of the filename
+    #eg. Pool25_S1_R1_001.fastq.gz
+    POOL=`echo $FILENAME | cut -d '_' -f 1 | tr --delete Pool `
+    SAMPLE=`echo $FILENAME | cut -d '_' -f 2 | tr --delete S ` 
+    REP=`echo $FILENAME | cut -d '_' -f 3 | tr --delete R ` 
 
-    #this works, but can't work out why the above doesn't work    
-    echo $HEADER | cut -d ':' -f 3,4,10 --output-delimiter=', ' >> headers.txt
-
-    #what I want to do is output a line
-    #1, Pool25_S1_R1_001.fastq.gz, H72NFAFXX, 1, CGATGT
-    #echo $count", "  $(basename "$file")", " $DETAILS  >> headers.txt
-
+    #now output the details to the text file
+    echo $INDEX',' $POOL',' $SAMPLE',' $REP',' $BARCODE',' $LANE',' $FILENAME >> headers.txt    
+  
 done
  
 echo "Process completed"
