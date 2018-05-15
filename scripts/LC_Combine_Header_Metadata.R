6eeec75347a662b1067f0ae7e35a0e2a359469ad
cla473 <Loretta.Clancy@csiro.au>
2018-05-16 07:25:51 +1000

    Modified script to use LEFT JOIN

--- scripts/LC_Combine_Header_Metadata.R
+++ scripts/LC_Combine_Header_Metadata.R
@@ -1,22 +1,24 @@
 #Script to merge Header Information with metadata
+rm(list=ls()) #remove existing objects in workspace
 
-headers <- read.csv("/OSM/CBR/AF_DATASCHOOL/output/metadata/headers.txt", header=T)
-#headers <- headers[order(headers$poolNo),]
-#df <- cbind(data, headers[,-4])
+#need to look at passing a path (ie "/OSM/CBR/AF_DATASCHOOL/input/Pools_metadata/")
+#so that we can set the working directory
+
+#setwd("/OSM/CBR/AF_DATASCHOOL/input/Pools_metadata/") 
+
+library(readxl)
+library(tidyverse)
 
 headers <- read.csv("/OSM/CBR/AF_DATASCHOOL/output/metadata/headers.txt", header=T)
 metadata <- read.csv("/OSM/CBR/AF_DATASCHOOL/output/metadata/metadata.csv", header=T)
 
-#mod cla473 - merge the data based on the "Pool" number, and then put data into different order.
-df <- merge(data, headers, by="Pool") %>% 
-  select (Pool, Plant_ID, Plate_ID, Index, Barcode, Lane_No, Sample_No,
-          Gen, Treatment, Date_of_treatment, sowing_date, sequence_platform, 
-          sequence_technique, sequence_date, processing_centre, Plant1, 
-          Plant2, Plant3, Plant4, Plant5, Plant6, Plant7, Plant8, Plant9,
-          Plant10, Plant11, Plant12, Total_Vol, Average_con, 
-          Filename, FilenamePath)
+#mod cla473 - merge the data based on the "Pool" number
+#             NOTE:  this needs to be a left join on the headers.txt as the metadata may
+#                    contain more information than we need
+merged.df <- headers %>% 
+  left_join(metadata, by="Pool") 
 
 
 #write_csv("to Pearcey directort.csv) FILL ME IN 
-write.csv(df, "/OSM/CBR/AF_DATASCHOOL/output/metadata/combined_data.csv", row.names=FALSE)
+write.csv(merged.df, "/OSM/CBR/AF_DATASCHOOL/output/metadata/combined_data.csv", row.names=FALSE)
 
