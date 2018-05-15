e17365fc1b1041e00a8b6f0f7761eb9799b5a1bc
Madeleine Barton <bar823@pearcey-i3.cm.hpc>
2018-05-15 14:53:51 +1000

    second attempt at sam->bam

--- scripts/MB_SAM_to_BAM.sh
+++ scripts/MB_SAM_to_BAM.sh
@@ -16,16 +16,14 @@ if [ -d "$INDIR" ]
 	# convert sam to sorted bam forma
 		if [ ! -z "$SLURM_ARRAY_TASK_ID" ]			
        		 then
+		 # Define filename to be called
 		 STEM=${IN_LIST["$SLURM_ARRAY_TASK_ID"]}
-        # cd ${IN_DIR} --- From Kerensa's script, should be able too run in same directory
-        # samtools fixmate -O bam ${STEM}*sam ${STEM}_fixmate.bam && rm ${STEM}*sam --- from Kerensa's script, 
-	# sequencing with paired ends? Not needed here...?
-        # samtools sort -O bam -@ 1 -m 8G -o ${STEM}_fixmate_sort.bam ${STEM}_fixmate.bam && rm ${STEM}_fixmate.bam
-        	 samtools sort -O bam ${STEM}*sam ${STEM}_sorted.bam 
-	# index does not read sam files so imporat as sam and first sort into a bam, write to *_sorted.bam
-	# samtools index ${STEM}_fixmate_sort.bam
-		 samtools index ${STEM}_sorted.bam > ${STEM}_sorted__indexed.bam && rm ${STEM}_sorted.bam
-	# write the outputs as a bam (-O bam), index, write to *indexed.bam..
+        	 # VIEW .sam file which converts it into a .bam file:[-b]=output is .bam; [S]=input is .sam (expects .bam as default)
+        	 samtools view -bS ${STEM}.sam > ${STEM}.bam
+		 # SORT the .bam files in case needed downstream  && remove unsorted.bam files to save space [O] = output is .bam	
+		 samtools sort -O bam ${STEM}.bam ${STEM}_sorted.bam && rm ${STEM}.bam 
+		 # INDEX the .bam files for Picard && remove the unindexed.bam files to save on space 
+		 samtools index ${STEM}_sorted.bam ${STEM}_sorted__indexed.bam && rm ${STEM}_sorted.bam
 		else
 		 echo "Error: Missing array index as SLURM_ARRAY_TASK_ID" 
 		fi
@@ -33,14 +31,10 @@ if [ -d "$INDIR" ]
          echo "Error: Missing array index as SLURM_ARRAY_TASK_ID"
  fi
 
-
-
 # For Submission:
-#
-# INDIR="/OSM/CBR/AF_DATASCHOOL/output/SB501086_0095_CHelliwell_CSIRO_Brapa_gDNA"       PUT YOUR DIRECTORY 
-LOCATION 
+# INDIR="/OSM/CBR/AF_DATASCHOOL/output/SB501086_0095_CHelliwell_CSIRO_Brapa_gDNA"  PUT YOUR DIRECTORY LOCATION 
 # outputHERE
 # NUM=$(expr $(ls -1 ${INDIR}/*.fastq.sam | wc -l) - 1)
-#sbatch -a 0-$NUM --export INDIR="$INDIR" SAM_to_BAM.sh#
+# sbatch -a 0-$NUM --export INDIR="$INDIR" SAM_to_BAM.sh#
 
  
