e535d2c2af3c9fd6a0281469a236266948afbdfe
Peter Watkins <Peter.Watkins@csiro.au>
2018-05-17 16:55:53 +1000

    Minor edits after local testing#

--- scripts/LC_BWA_alignment_v2.sh
+++ scripts/LC_BWA_alignment_v2.sh
@@ -10,41 +10,40 @@ module load bwa
 #TODO:
 #  The INFILE and GENOME will need to be passed in - *DONE* PW
 #  Needs to be ran on HPC (currently works locally) (and details put in here as comments)
-#  Trial comments added
+#  Trial comments added below
 
 
 #PW - added input for parameters to pass to script
-#if [ -z "$1" ] ; ## Check for provided input - first, assumed to be genome
-#  then
-#    echo "* Error * -> please provide genome location and name"
-#    exit 1
-#  else
+if [ -z "$1" ] ; ## Check for provided input - first, assumed to be genome
+  then
+    echo "* Error * -> please provide genome location and name"
+    exit 1
+  else
 GENOME=$1
-#fi
+fi
 
-#if [ -z "$2" ] ; ## Another check - this time for metadata
-#  then
-#    echo "* Error * -> provide location and name for metadata"
-#    exit 1
-#  else
+if [ -z "$2" ] ; ## Another check - this time for metadata
+  then
+    echo "* Error * -> provide location and name for metadata"
+    exit 1
+  else
 INFILE=$2
-#fi
+fi
 
 #for testing purposes only
 #INFILE="/OSM/CBR/AF_DATASCHOOL/output/metadata/combined_data.csv"
 #GENOME="/OSM/CBR/AF_DATASCHOOL/input/genome/GCF_000686985.2_Bra_napus_v2.0_genomic.fna"
 
-
 #NOTE:  The output directory is same as filenamePath directory except that 'input' is replace with 'output'
 
 #need to check what we need here as far as validating the file goes
 #if [ -f "$INFILE" ] && { echo "$INFILE file not found"; exit 99; }
-if [ -f "$INFILE" ] 
-then
+if [ -f "$INFILE" ] ;
+  then
 
     cat $INFILE | while read LINE
     do
-        #echo $LINE
+#        echo $LINE
 
         #columns are:
         #   (1) Index, (2)Barcode, (3)Lane_No, (4)Pool, (5)Sample_No, (6)Filename, 
@@ -64,18 +63,20 @@ then
         SEQDATE=`echo $LINE | cut -d ',' -f10 | sed -e 's/^"//' -e 's/"$//' `   
         PROCCENTRE=`echo $LINE | cut -d ',' -f11 | sed -e 's/^"//' -e 's/"$//' `
 
-        ID=`echo "${INDEX}.${LANE}.${INDEX}"`
+        ID=`echo "${BARCODE}.${LANE}.${INDEX}"` ## Double reference to index
         DS=`echo "Pool_${POOL}.Sample_${SAMPLE}"`
         PU=`echo "${BARCODE}.${LANE}"` 
-
-        OUTDIR=`echo $(dirname ${FILEPATH}) | sed -e 's/input/output/' `
-        echo ${OUTDIR}
+        OUTDIR=`echo $(dirname ${FILEPATH}) | sed -e 's/input/output/' `  ## Changes 'input' to 'output' in filename - different paths present in data set
+#        echo ${OUTDIR}
 
         #now we are ready to build the full string to be passed as a parametre for  BWA
         RG1=`echo "@RG\tID:${ID}$\tBC:${INDEX}\tCN:${PROCCENTRE}\tDS:${DS}\tDT:${SEQDATE}\t"`
-        RG2=`echo "${RG1}LB:${BARCODE}\tPG:${TECHNIQUE}\tPL:${PLATFORM}\tPM:{TECHNIQUE}\t"`
+        RG2=`echo "${RG1}LB:${BARCODE}\tPG:${TECHNIQUE}\tPL:${PLATFORM}\tPM:${TECHNIQUE}\t"`
         RG=`echo "${RG2}PU:${PU}\tSM:${POOL}" `
         echo ${RG}   
+
+## After building @RG 'Read Group', parse as string to BWA for processing....
+
 
         if [ ! -f ${OUTDIR}/${FILENAME}.sam ]
         then
@@ -88,7 +89,7 @@ then
                   -M \
                   -r ${RG} > ${OUTDIR}/${FILENAME}.sam 2>> ${OUTDIR}/${FILENAME}.log
         fi
-    done 
+   done 
 fi
 
 exit 0
@@ -99,6 +100,6 @@ exit 0
 # INFILE="/OSM/CBR/AF_DATASCHOOL/output/combined_data.csv"
 # GENOME="/OSM/CBR/AF_DATASCHOOL/input/genome/GCF_00068695.2_Bra_napus_v2.0_genomic.fna"
 # NUM=`expr $(wc -l $INFILE | cut -d" " -f1) - 1`
-# sbatch -a -0-$NUM --export GENOME=$GENOME" INFILE="$INFILE" LC_BWA_alignment_v2.sh
+# sbatch -a -0-$NUM --export GENOME=$GENOME" --export INFILE="$INFILE" LC_BWA_alignment_v2.sh
 
 
