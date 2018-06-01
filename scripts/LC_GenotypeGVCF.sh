62bf0b1e0f65d2fea223fba36801599ab36619fe
cla473 <Loretta.Clancy@csiro.au>
2018-06-01 11:27:12 +1000

    added latest version of file

--- scripts/LC_GenotypeGVCF.sh
+++ scripts/LC_GenotypeGVCF.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 #SBATCH --job-name=Run_GenotypeGVCFs
-#SBATCH --time=24:00:00
+#SBATCH --time=72:00:00
+#SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --mem=20g
+#SBATCH --mem=5g
 
 module load gatk/4.0.4.0
  
@@ -13,7 +14,7 @@ GENOME="/OSM/CBR/AF_DATASCHOOL/input/ref_genome/GCF_000686985.2_Bra_napus_v2.0_g
 # Clear our log file each time
 #echo "Processing started... " > ${FILEDIR}/genotypeGVCF.log
 
-gatk GenotypeGVCFs -R ${GENOME} --variant ${FILEDIR}/new_all_gvcf_raw.vcf -O ${FILEDIR}/output.vcf 
+gatk GenotypeGVCFs -R ${GENOME} --variant ${FILEDIR}/new_all_gvcf_raw.vcf -O ${FILEDIR}/output_v2.vcf 
  
 #echo "Process completed" >> ${FILEDIR}/genotypeGVCF.log
 
