5894d67dccaadc873f116b247eea6c9961f6d140
cla473 <Loretta.Clancy@csiro.au>
2018-06-12 08:49:16 +1000

    Added updated GenotypeGVCF script

---
+++ scripts/LC_GenotypeGVCF_hli001.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#SBATCH --job-name=Run_GenotypeGVCFs
+#SBATCH --time=1-0:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --mem=48g
+
+module load gatk/4.0.4.0
+ 
+FILEDIR="/OSM/CBR/AF_DATASCHOOL/output/2018-05-03_canola/GVCF"
+GENOME="/OSM/CBR/AF_DATASCHOOL/input/ref_genome/GCF_000686985.2_Bra_napus_v2.0_genomic.fasta"
+
+# Clear our log file each time
+#echo "Processing started... " > ${FILEDIR}/genotypeGVCF.log
+
+export  JAVA_TOOL_OPTIONS=-Xmx46g
+
+gatk GenotypeGVCFs --new-qual -R ${GENOME} --variant ${FILEDIR}/new_all_gvcf_raw.vcf -O ${FILEDIR}/output_v0.vcf 
+ 
+#echo "Process completed" >> ${FILEDIR}/genotypeGVCF.log
+
+# To run on Command Line:
+#./LC_GenotypeGVCF.sh 
+
+# To run on SLURM
+#sbatch LC_GenotypeGVCF.sh 
